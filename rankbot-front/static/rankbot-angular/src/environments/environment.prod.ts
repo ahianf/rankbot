2522568523587a6ab19c4619f9e7cdf7474061d7
ahianf <ahian.fernandez@outlook.com>
2023-08-17 12:50:42 -0400

    nginx auth pt.3, angular prod settings

--- rankbot-front/static/rankbot-angular/src/environments/environment.prod.ts
+++ rankbot-front/static/rankbot-angular/src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
-  production: true
+  production: true,
+  authorize_uri: 'https://auth.rankmachine.me/oauth2/authorize?',
+  client_id : 'client',
+  redirect_uri: 'https://rankmachine.me/authorized',
+  scope: 'openid profile',
+  response_type: 'code',
+  response_mode: 'form_post',
+  code_challenge_method: 'S256',
+  token_url: 'https://auth.rankmachine.me/oauth2/token',
+  grant_type: 'authorization_code',
+  resource_url: 'https://rankmachine.me/api/v2/',
+  api_v1: 'https://rankmachine.me/api/',
+  logout_url: 'https://auth.rankmachine.me/logout',
+  secret_pkce: 'secret'
 };
