4e2a355766e2fbac51c28cf67d5a5497e0ce8665
ahianf <ahian.fernandez@outlook.com>
2023-08-17 21:10:51 -0400

    final rankbot v3, fuck google

--- rankbot-front/static/rankbot-angular/src/environments/environment.prod.ts
+++ rankbot-front/static/rankbot-angular/src/environments/environment.prod.ts
@@ -1,16 +1,16 @@
 export const environment = {
   production: true,
-  authorize_uri: 'https://auth.rankmachine.me/oauth2/authorize?',
+  authorize_uri: 'https://rankmachine.me/auth/oauth2/authorize?',
   client_id : 'client',
   redirect_uri: 'https://rankmachine.me/authorized',
   scope: 'openid profile',
   response_type: 'code',
   response_mode: 'form_post',
   code_challenge_method: 'S256',
-  token_url: 'https://auth.rankmachine.me/oauth2/token',
+  token_url: 'https://rankmachine.me/auth/oauth2/token',
   grant_type: 'authorization_code',
   resource_url: 'https://rankmachine.me/api/v2/',
-  api_v1: 'https://rankmachine.me/api/',
-  logout_url: 'https://auth.rankmachine.me/logout',
+  api_v1: 'http://localhost:8080/api/',
+  logout_url: 'https://rankmachine.me/auth/logout',
   secret_pkce: 'secret'
 };
