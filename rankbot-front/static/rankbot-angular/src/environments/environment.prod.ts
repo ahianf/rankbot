655d4453239cddeeb03ea39786fea5a4f1713f97
ahianf <ahian.fernandez@outlook.com>
2023-08-17 21:42:07 -0400

    modifications

--- rankbot-front/static/rankbot-angular/src/environments/environment.prod.ts
+++ rankbot-front/static/rankbot-angular/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   resource_url: 'https://rankmachine.me/api/v2/',
-  api_v1: 'http://localhost:8080/api/',
+  api_v1: 'https://rankmachine.me/api/',
 };
